56e900edc6e20bb22cff47129c9b5e2e692b844e
Daria <spodina.darya@gmail.com>
2023-08-09 18:54:21 +0200

    Fixed margin for the main page

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,13 @@ export default function Home() {
       <div className="container mx-auto">
         <div className="flex flex-row justify-around">
           <div className="flex flex-col items-center">
-            <div className="text-3xl tracking-wide font-extralight py-4 mb-10 max-w-md text-center">
+            <div className="text-3xl tracking-wide font-extralight py-0 mb-10 max-w-md text-center">
               <h2>Let's get ready for grocery shopping!</h2>
             </div>
             <ProductInput />
             <AddButton />
           </div>
-          <div className="container bg-white rounded-lg min-w-500px w-1/3 mb-24">
+          <div className="container bg-white rounded-lg min-w-500px w-1/3 mt-[-70px] mb-24">
             <GroceryList />
           </div>
         </div>
